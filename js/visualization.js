cad3080107a0abe2a425bc2b7309d7cf8a771a23
themsouM <mariksoum@gmail.com>
2018-09-24 16:26:38 +0300

    have no clue whats done here

--- js/visualization.js
+++ js/visualization.js
@@ -1,30 +1,32 @@
-fetch("http://data.fixer.io/api/latest?access_key=900a6a062b01085bd70009074858c743")
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (myJson) {
-        let res = (myJson);
-        // console.log(res);
-
-        Object.keys(res["rates"]).forEach(function (key) {
-            // console.log('Key : ' + key);
-            // console.log(res["rates"][key]);
-            document.getElementsByClassName("content__curr-name")[0].innerHTML += key + ": " + res["rates"][key].toFixed(3) + '<br/>';
-            // document.getElementsByClassName("content__curr-value")[0].innerHTML += res["rates"][key];
-        });
-    });
-
-
 const url = "http://data.fixer.io/api/latest?access_key=900a6a062b01085bd70009074858c743";
 
 const fetchData = (url) => {
+    return fetch(url)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (myJson) {
+            let promise1 = Promise.resolve(myJson["rates"]);
+
+            promise1.then(function (value) {
+                console.log(value);
+            });
+            // return myJson;
+        });
 
 };
 
-
-
 const currencies = fetchData(url);
-
+console.log(currencies);
 
 const renderCurrencies = (curr) => {
-}
+    console.log(curr);
+    Object.keys(curr["rates"]).forEach(function (key) {
+        // console.log('Key : ' + key);
+        // console.log(res["rates"][key]);
+        document.getElementsByClassName("content__curr-name")[0].innerHTML += key + ":";
+        document.getElementsByClassName("content__curr-value")[0].innerHTML += curr["rates"][key].toFixed(3);
+    });
+};
+
+console.log(renderCurrencies(currencies));
